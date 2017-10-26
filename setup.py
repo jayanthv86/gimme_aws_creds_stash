d37c5c7324bb4abe18467b437a466c80ce4ee0e0
Jayanth Venkatraj <jay.venkatraj@okta.com>
2017-10-25 23:39:51 -0400

    Init

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='gimme aws creds',
+    version='1.0.1',
+    install_requires=requirements,
+    author='Ann Wallace',
+    author_email='ann.wallace@nike.com',
+    description="A CLI to get temporary AWS credentials from Okta",
+    url='https://github.com/Nike-Inc/gimme-aws-creds',
+    license='Apache License, v2.0',
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite="tests",
+    scripts=['bin/gimme-aws-creds'],
+)
